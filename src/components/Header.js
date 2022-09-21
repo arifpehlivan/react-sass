074668a54d6352cb2eac34df6a5a68606db8f0e4
arifpehlivan <arifpehlivanapap@gmail.com>
2022-09-21 13:50:56 +0300

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import { MenuOutlined } from '@material-ui/icons'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./styles/Header.scss"
+
+const Hedaer = () => {
+    return (
+        <div className='header'>
+            <nav className='nav-container'>
+                <div className="menu">
+                    <MenuOutlined className='menu-icon'/>
+                </div>
+                <div className="logo">
+                    <h1>Travel</h1>
+                </div>
+                <div className="right">
+                    <ul>
+                        <li>
+                            <Link to="/places">Places</Link>
+                        </li>
+                        <li>
+                            <Link to="/pricing">Pricing</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">Sign Up</Link>
+                        </li>
+                        <li>
+                            <Link to="/sponsors">Sponsors</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Hedaer
