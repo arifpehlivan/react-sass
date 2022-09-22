47034dd97e9a3921a76839e2a76f01a45f072ea5
arifpehlivan <arifpehlivanapap@gmail.com>
2022-09-22 09:07:04 +0300

    add gallery

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import Header from "./components/Header"
 import Section from "./components/Section"
 import {Data} from "./components/Data"
+import Second from './components/Second';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Section slides={Data}/>
+      <Second/>
     </div>
   );
 }
